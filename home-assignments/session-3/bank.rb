607e3f9a1cce4361e4916ad49914a15a914785a1
gita <gitaa@gmail.com>
2016-10-16 10:07:51 +0300

    Fixed bank.rb after Eyal notes

--- home-assignments/session-3/bank.rb
+++ home-assignments/session-3/bank.rb
@@ -1,12 +1,7 @@
 # print_action method expects two parameters "action" and "amount"
 #“print_action” method in this code serves frist rule of “coding practices for beginners” : Dry "Do not repeat yourslef"
-def print_action(action,amount,name,balance)
-  if (action!="view")
+def print_action(action,amount)
     puts "You've asked to #{action} an amount of #{amount}$. Performing your request..."
-  else
-    puts "You've asked to #{action} your balance. Performing your request..."
-  end
-  puts "Hi #{name}, Your current balance is #{balance}"
 end
 
 balance = rand(100..1000)
@@ -16,24 +11,25 @@ name = gets.chomp.capitalize!
 
 print "What action would you like to perform?  "
 action = gets.chomp
+
 if (action!="deposit" && action!="withdraw" && action!="view")
 raise  "Action not valid! Please choose deposit/withdraw/view"
-end
-
-if (action!="view")
+elsif (action!="view")
 print "What amount  would you like to #{action}?  "
 amount = gets.to_i
+print_action(action,amount)
+else
+puts "You've asked to #{action} your balance. Performing your request..."
 end
 
-print_action(action,amount,name,balance)
+puts "Hi #{name}, Your current balance is #{balance}"
 
 if action == "withdraw"
     balance -= amount
   elsif action == "deposit"
     balance += amount
   elsif action == "view"
+  exit
 end
 
-if (action!="view")
 puts "Hi #{name}, Your current balance was changed and is now: #{balance}$"
-end
