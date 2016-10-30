8c77e416032c77d02a78d9e5a3ad103f7d9bd3d0
gita <gitaa@gmail.com>
2016-10-30 21:59:56 +0200

    Session4 home work animals new file

---
+++ home-assignments/session-4/animals.rb
@@ -0,0 +1,28 @@
+  animals_list=[]
+
+  class Animal
+  attr_reader :name, :color
+  def initialize(name, color)
+  @name = name
+  @color = color
+  end
+  end
+  animals_list = [Animal.new("rabbit","gray"),Animal.new("mouse","gray"),
+    Animal.new("deer","brown"),Animal.new("bear","brown"),Animal.new("sheep","white"),Animal.new("flamingo","pink"),
+    Animal.new("beetle","red"),Animal.new("duck","white"),Animal.new("ant","black"),Animal.new("donkey","brown"),
+    Animal.new("dolphin","gray")]
+
+    puts "List of animals with number of characters in their name smaller than 5 "
+    animals_list.each do |animal|
+    puts  animal.name.length < 5 ? "#{animal.name}":""
+    end
+
+    puts "List of animals with the color “brown"
+    animals_list.each do |animal|
+    puts animal.color == "brown" ? "#{animal.name}":""
+    end
+
+    puts "Animals  color is gray"
+    animals_list.each do |animal|
+    puts animal.color == "gray" ? "Wow!":""
+    end
